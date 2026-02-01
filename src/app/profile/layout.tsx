cb08f66aa37d6dfe402df3128be9eb4739d60cc3
Venura Wijesekera <venuraws@gmail.com>
2026-02-01 23:46:03 +0530

    latest

---
+++ src/app/profile/layout.tsx
@@ -0,0 +1,16 @@
+import Sidebar from "@/components/dashboard/Sidebar";
+
+export default function ProfileLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className="flex flex-col md:flex-row min-h-screen md:h-screen md:overflow-hidden bg-[#0f0f0f]">
+            <Sidebar />
+            <main className="flex-1 relative overflow-y-auto bg-[#050505]">
+                {children}
+            </main>
+        </div>
+    );
+}
